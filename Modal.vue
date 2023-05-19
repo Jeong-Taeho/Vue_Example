411dc66ca946995e10be52091fc0087f433c708c
Jeong-Taeho <117630589+Jeong-Taeho@users.noreply.github.com>
2023-05-19 18:19:37 +0900

    Vue동산 만들어보기(props)

---
+++ Modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="black-bg" v-if="openmodal">
+    <div class="white-bg">
+      <h4>{{products[modalclick].title}}</h4>
+      <p>{{products[modalclick].price}}만원</p>
+      <!-- <button class="modal-btn" @click="openmodal = false">close</button>    -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Modal-item',
+    props : {
+        products : Array,
+        modalclick : Number,
+        openmodal : Boolean,
+    }
+}
+</script>
+
+<style>
+
+</style>
